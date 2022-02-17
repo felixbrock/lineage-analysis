2ef3a849ea6a4af03ae6379802d5c9ec5e055ea9
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-02-17 22:03:52 +0100

    structural changes

--- test.py
+++ test.py
@@ -1,79 +1,4 @@
-# import sqlfluff
-# import json
-
-# fd = open('table2.sql', 'r')
-# sqlFile = fd.read()
-# # print(sqlFile)
-# fd.close()
-
-# # my_bad_query = "SeLEct  *, 1, blah as  fOO  from mySchema.myTable"
-# # query2 = "with customers as (    select * from {{ ref('stg_customers') }}), orders as (    select * from {{ ref('stg_orders') }}),customer_orders as (   select       customer_id,        min(order_date) as first_order_date,        max(order_date) as most_recent_order_date,        count(order_id) as number_of_orders    from orders group by 1), final as (    select        customers.customer_id,        customers.first_name,        customers.last_name,        customer_orders.first_order_date,        customer_orders.most_recent_order_date,        coalesce(customer_orders.number_of_orders, 0) as number_of_orders   from customers    left join customer_orders using (customer_id))select * from final"
-
-# # fix_result_1 = sqlfluff.fix(sqlFile, dialect="snowflake")
-# # print(fix_result_1)
-
-# parseResult = sqlfluff.parse(sqlFile, 'snowflake')
-
-# # json = json.dumps(parse_result)
-# # print()
-
-# def appendPath(key, path):
-#   if not path:
-#     path += key
-#   else:
-#     path += f'.{key}'
-#   return path
-
-# def dictionaryExtract(key, var, path = ''):
-#   if hasattr(var,'items'):
-#     results = []
-#     for k, v in var.items():
-#       if k == key:
-#         path = appendPath(k, path)
-#         results.append((path, v))
-#       if isinstance(v, dict):
-#         for result in dictionaryExtract(key, v, appendPath(k, path)):
-#           results.append(result)
-#       elif isinstance(v, list):
-#         for d in v:
-#           for result in dictionaryExtract(key, d, appendPath(k, path)):
-#             results.append(result)
-#     return results
-
-# # enum ReferencingTypes ={]}
-
-# class ReferenceAnalyzer:
-#   def __init__(self, referencingStatements) -> None:
-#       self.referencingStatements = referencingStatements
-
-#   def analyze(self, key, statementIndex):
-#     createTableStatement = 'create_table_statement'
-
-#     match key:
-#       case createTableStatement:
-#         referencingStatement = self.referencingStatements[statementIndex]
-#         tableToCreate = self.referencingStatements
-#         print('table to create')
-
-# referencingStatements = []
-
-# file = parseResult['file']
-# statementKey = 'statement'
-# identifierKey = 'identifier'
-
-# if isinstance(file, dict) and statementKey in file:
-#   referencingStatements.append(dictionaryExtract(identifierKey, file[statementKey]))
-# elif isinstance(file, list):
-#   for statement in file:
-#     if not statementKey in statement:
-#         continue
-#     referencingStatements.append(dictionaryExtract(identifierKey, statement[statementKey]))
-
-# print(referencingStatements)
-
 from enum import Enum
-from itertools import count
-from tkinter.tix import COLUMN
 import sqlfluff
 import json
 
@@ -93,47 +18,8 @@ parseResult = sqlfluff.parse(sqlFile, 'snowflake')
 # json = json.dumps(parse_result)
 # print()
 
-def appendPath(key, path):
-  if not path:
-    path += key
-  else:
-    path += f'.{key}'
-  return path
 
 
-# def dictionaryExtract(key, var, path = ''):
-#   if hasattr(var,'items'):
-#     results = []
-#     for k, v in var.items():
-#       if k == key:
-#         path = appendPath(k, path)
-#         results.append((path, v))
-#       if isinstance(v, dict):
-#         for result in dictionaryExtract(key, v, appendPath(k, path)):
-#           results.append(result)
-#       elif isinstance(v, list):
-#         for d in v:
-#           for result in dictionaryExtract(key, d, appendPath(k, path)):
-#             results.append(result)
-#     return results
-
-def dictionaryExtract(key, var, path = ''):
-  if hasattr(var,'items'):
-    results = {}
-    for k0, v0 in var.items():
-      if k0 == key:
-        path = appendPath(k0, path)
-        results[path] = v0
-      if isinstance(v0, dict):
-        for k1,v1 in dictionaryExtract(key, v0, appendPath(k0, path)).items():
-          results[k1] = v1
-      elif isinstance(v0, list):
-        for d in v0:
-          for k2, v2 in dictionaryExtract(key, d, appendPath(k0, path)).items():
-            results[k2] = v2
-    return results
-
-# enum ReferencingTypes ={]}
 
 class SQLElement(Enum):
   FILE = 'file'
@@ -168,32 +54,45 @@ class LineageInformation(Enum):
 
 
 class DependencyAnalyzer:
-  def __init__(self, referencingStatements) -> None:
-      self.referencingStatements = referencingStatements
+  
+  def __init__(self) -> None:
+      self.dependencyStatements = []
 
-  def isColumnReference(self, key):
+  def __appendPath(self, key, path):
+    if not path:
+      path += key
+    else:
+      path += f'.{key}'
+    return path
+
+  def addDependencyStatement(self, dependencyStatement):
+    self.dependencyStatements.append(dependencyStatement)
+
+  def extractDependencyStatement(self, targetKey, var, path = ''):
+    if hasattr(var,'items'):
+      results = {}
+      for k0, v0 in var.items():
+        if k0 == targetKey:
+          path = self.__appendPath(k0, path)
+          results[path] = v0
+        if isinstance(v0, dict):
+          for k1,v1 in self.extractDependencyStatement(targetKey, v0, self.__appendPath(k0, path)).items():
+            results[k1] = v1
+        elif isinstance(v0, list):
+          for d in v0:
+            for k2, v2 in self.extractDependencyStatement(targetKey, d, self.__appendPath(k0, path)).items():
+              results[k2] = v2
+      return results
+
+  def isColumnDependency(self, key):
     return SQLElement.COLUMN_REFERENCE.value in key
-
-  # def __analyze(self, key, statementIndex):
-
-
-
-  #         # tableToCreateRef = f'{SQLElement.CREATE_TABLE_STATEMENT}.{SQLElement.TABLE_REFERENCE}.{SQLElement.IDENTIFIER}'
-  #         # if not tableToCreateRef in referencingStatement:
-  #         #   raise LookupError(f'{tableToCreateRef} not found')
-
-  #         # return {LineageInformation.TABLE_TO_CREATE: self.referencingStatements[tableToCreateRef]}
 
   def analyzeColumnDependency(self, key, statementIndex):
 
-    if not (self.isColumnReference(key)):
+    if not (self.isColumnDependency(key)):
         return
 
-    # lineageInfo = {}
-
-    # result = self.__analyze(key, statementIndex)
-
-    referencingStatement = self.referencingStatements[statementIndex]
+    referencingStatement = self.dependencyStatements[statementIndex]
 
     result = {}
 
@@ -236,28 +135,26 @@ class DependencyAnalyzer:
     return result
       
 
-referencingStatements = []
+analyzer = DependencyAnalyzer()
 
 file = parseResult[SQLElement.FILE.value]
 
 if isinstance(file, dict) and SQLElement.STATEMENT.value in file:
-  referencingStatements.append(dictionaryExtract(SQLElement.IDENTIFIER.value, file[SQLElement.STATEMENT.value]))
+  dependencyStatement = analyzer.extractDependencyStatement(SQLElement.IDENTIFIER.value, file[SQLElement.STATEMENT.value])
+  analyzer.addDependencyStatement(dependencyStatement)
 elif isinstance(file, list):
   for statement in file:
     if not SQLElement.STATEMENT.value in statement:
         continue
-    referencingStatements.append(dictionaryExtract(SQLElement.IDENTIFIER.value, statement[SQLElement.STATEMENT.value]))
-
-print(referencingStatements)
-
-analyzer = DependencyAnalyzer(referencingStatements)
+    dependencyStatement = analyzer.extractDependencyStatement(analyzer.extractDependencies(SQLElement.IDENTIFIER.value, statement[SQLElement.STATEMENT.value]))
+    analyzer.addDependencyStatement(dependencyStatement)
 
 lineageInfo = []
 
 counter = 0
-for statement in referencingStatements:
+for statement in analyzer.dependencyStatements:
   for k,v in statement.items():
-    if not analyzer.isColumnReference(k):
+    if not analyzer.isColumnDependency(k):
       continue
 
     result = analyzer.analyzeColumnDependency(k, counter)
