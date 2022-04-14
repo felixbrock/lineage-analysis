3988f1bc4ec662b2d05f6c372b8b1e21df16e10a
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-04-14 10:50:36 +0200

    added self information in statement creation

--- src/domain/value-types/logic.ts
+++ src/domain/value-types/logic.ts
@@ -1,21 +1,32 @@
 import SQLElement from './sql-element';
 
-export enum ReferenceType {
+enum RefType {
   TABLE = 'TABLE',
   COLUMN = 'COLUMN',
-  WILDCARD = 'WILDCARD',
 }
 
-export interface StatementReference {
+interface Ref {
   path: string;
-  type: ReferenceType;
-  columnName?: string;
+  name: string;
+  isSelfRef: boolean;
+}
+
+type TableRef = Ref;
+
+interface ColumnRef extends Ref {
+  isWildcardRef: boolean;
   tableName?: string;
+}
+
+export interface Refs {
+  tables: TableRef[];
+  columns: ColumnRef[];
+  wildcards: ColumnRef[];
 }
 
 interface LogicProperties {
   parsedLogic: string;
-  statementReferences: StatementReference[][];
+  statementReferences: Refs[];
 }
 
 export interface LogicPrototype {
@@ -31,13 +42,13 @@ interface HandlerProperties<ValueType> {
 export class Logic {
   #parsedLogic: string;
 
-  #statementReferences: StatementReference[][];
+  #statementReferences: Refs[];
 
   get parsedLogic(): string {
     return this.#parsedLogic;
   }
 
-  get statementReferences(): StatementReference[][] {
+  get statementReferences(): Refs[] {
     return this.#statementReferences;
   }
 
@@ -52,37 +63,56 @@ export class Logic {
     return newPath;
   };
 
-  static #handleIdentifierRef = (
+  // Checks if reference is a self reference and, hence, defines the target column and table itself
+  static #isSelfRef = (path: string, refType: RefType): boolean => {
+    const columnSelfRefs =
+      refType === RefType.TABLE
+        ? [
+            `${SQLElement.CREATE_TABLE_STATEMENT}.${SQLElement.TABLE_REFERENCE}.${SQLElement.IDENTIFIER}`,
+            `${SQLElement.FROM_EXPRESSION_ELEMENT}.${SQLElement.TABLE_EXPRESSION}.${SQLElement.TABLE_REFERENCE}.${SQLElement.IDENTIFIER}`,
+          ]
+        : [
+            `${SQLElement.SELECT_CLAUSE_ELEMENT}.${SQLElement.COLUMN_REFERENCE}.${SQLElement.IDENTIFIER}`,
+            `${SQLElement.COLUMN_DEFINITION}.${SQLElement.IDENTIFIER}`,
+            `${SQLElement.SELECT_CLAUSE_ELEMENT}.${SQLElement.WILDCARD_EXPRESSION}.${SQLElement.WILDCARD_IDENTIFIER}`,
+          ];
+
+    return columnSelfRefs.some((ref) => path.includes(ref));
+  };
+
+  // Handles any column reference
+  static #handleColumnIdentifierRef = (
     props: HandlerProperties<string>
-  ): StatementReference => {
-    const type = props.referencePath.includes(SQLElement.TABLE_REFERENCE)
-      ? ReferenceType.TABLE
-      : ReferenceType.COLUMN;
-
-    const columnValueReference =
-      type === ReferenceType.COLUMN
-        ? this.#getColumnValueReference(props.value)
-        : undefined;
-
-    let columnName;
-    if (columnValueReference) columnName = columnValueReference.columnName;
-    else if (type === ReferenceType.COLUMN) columnName = props.value;
-
-    let tableName;
-    if (columnValueReference) tableName = columnValueReference.tableName;
-    else if (type === ReferenceType.TABLE) tableName = props.value;
+  ): ColumnRef => {
+    const columnValueRef = this.#getColumnValueReference(props.value);
+    const path = this.#appendPath(props.key, props.referencePath);
 
     return {
-      path: this.#appendPath(props.key, props.referencePath),
-      type,
-      columnName,
-      tableName,
+      isSelfRef: this.#isSelfRef(path, RefType.COLUMN),
+      path,
+      name: columnValueRef.columnName,
+      tableName: columnValueRef.tableName,
+      isWildcardRef: false,
     };
   };
 
+  // Handles any table reference
+  static #handleTableIdentifierRef = (
+    props: HandlerProperties<string>
+  ): TableRef => {
+    const path = this.#appendPath(props.key, props.referencePath);
+
+    return {
+      isSelfRef: this.#isSelfRef(path, RefType.TABLE),
+      path,
+      name: props.value,
+    };
+  };
+
+  // Handles any case of * (wildcard) references
   static #handleWildCardRef = (
     props: HandlerProperties<{ [key: string]: any }>
-  ): StatementReference => {
+  ): ColumnRef => {
     const wildcardElementKeys = Object.keys(props.value);
 
     const isDotNoation = [
@@ -93,74 +123,54 @@ export class Logic {
       wildcardElementKeys.includes(wildcardElementKey)
     );
 
+    const path = this.#appendPath(props.key, props.referencePath);
+
     if (isDotNoation)
       return {
-        path: this.#appendPath(props.key, props.referencePath),
-        type: ReferenceType.WILDCARD,
+        isSelfRef: true,
+        path,
+        name: SQLElement.WILDCARD_IDENTIFIER_STAR,
         tableName: props.value[SQLElement.WILDCARD_IDENTIFIER_IDENTIFIER],
+        isWildcardRef: true,
       };
     if (
       wildcardElementKeys.length === 1 &&
       wildcardElementKeys.includes(SQLElement.WILDCARD_IDENTIFIER_STAR)
     )
       return {
-        path: this.#appendPath(props.key, props.referencePath),
-        type: ReferenceType.WILDCARD,
+        isSelfRef: true,
+        path,
+        name: SQLElement.WILDCARD_IDENTIFIER_STAR,
+        isWildcardRef: true,
       };
     throw new SyntaxError('Unhandled wildcard-dict use-case');
   };
 
-  static #handleColumnRef = (
-    props: HandlerProperties<{ [key: string]: string }[]>
-  ): StatementReference => {
-    const subStatementReferences = props.value
-      .map((valueElement) =>
-        this.#extractstatementReferences(
-          valueElement,
-          this.#appendPath(props.key, props.referencePath)
-        )
-      )
-      .flat();
-
-    if (!subStatementReferences.length)
-      throw new RangeError(
-        'Array based column statement does not have any elements'
-      );
-
-    const elements = subStatementReferences.map(
-      (statement) => statement.columnName
-    );
-
-    if (!elements.length)
-      throw new RangeError('Column reference value elements not found');
-
-    return {
-      path: subStatementReferences[0].path,
-      type: ReferenceType.COLUMN,
-      tableName: elements.length > 1 ? elements[0] : undefined,
-      columnName: elements.length > 1 ? elements[1] : elements[0],
-    };
-  };
-
+  // Splits up table.column notation
   static #getColumnValueReference = (
     columnValue: string
-  ): { columnName?: string; tableName?: string } => {
+  ): { columnName: string; tableName?: string } => {
     const columnName = columnValue.includes('.')
       ? columnValue.split('.').slice(-1)[0]
       : columnValue;
     const tableName = columnValue.includes('.')
       ? columnValue.split('.').slice(0)[0]
-      : '';
+      : undefined;
 
     return { columnName, tableName };
   };
 
-  static #extractstatementReferences = (
+  // Runs through parse SQL logic (JSON object) and check for potential dependencies. Return those dependencies.
+  static #extractReferences = (
     parsedSQL: { [key: string]: any },
     path = ''
-  ): StatementReference[] => {
+  ): Refs => {
     let referencePath = path;
-    const statementReferences: StatementReference[] = [];
+    const refs: Refs = {
+      columns: [],
+      tables: [],
+      wildcards: [],
+    };
 
     Object.entries(parsedSQL).forEach((parsedSQLElement) => {
       const key = parsedSQLElement[0];
@@ -172,62 +182,87 @@ export class Logic {
           : referencePath;
 
       if (key === SQLElement.IDENTIFIER)
-        statementReferences.push(
-          this.#handleIdentifierRef({ key, value, referencePath })
-        );
-      else if (key === SQLElement.WILDCARD_IDENTIFIER){
-        statementReferences.push(
-          this.#handleWildCardRef({ key, value, referencePath })
-        );
-      }
-      else if (value.constructor === Object) {
-        const subStatementReferences = this.#extractstatementReferences(
-          value,
-          this.#appendPath(key, referencePath)
-        );
-        subStatementReferences.forEach((reference) =>
-          statementReferences.push(reference)
-        );
-      } else if (Object.prototype.toString.call(value) === '[object Array]') {
-        if (key === SQLElement.COLUMN_REFERENCE)
-          statementReferences.push(
-            this.#handleColumnRef({ key, value, referencePath })
+        if (path.includes(SQLElement.COLUMN_REFERENCE))
+          refs.columns.push(
+            this.#handleColumnIdentifierRef({ key, value, referencePath })
           );
-        else
-          value.forEach((valueElement: { [key: string]: any }) => {
-            const subStatementReferences = this.#extractstatementReferences(
-              valueElement,
-              this.#appendPath(key, referencePath)
-            );
-            subStatementReferences.forEach((reference) =>
-              statementReferences.push(reference)
-            );
+        else if (path.includes(SQLElement.TABLE_REFERENCE)) {
+          const ref = this.#handleTableIdentifierRef({
+            key,
+            value,
+            referencePath,
           });
-      }
+
+          if (
+            ref.isSelfRef &&
+            refs.tables.filter((table) => table.isSelfRef).length
+          )
+            throw new RangeError(
+              'Multiple self references of model materialization found'
+            );
+
+          refs.tables.push(ref);
+        } else if (key === SQLElement.WILDCARD_IDENTIFIER)
+          refs.wildcards.push(
+            this.#handleWildCardRef({ key, value, referencePath })
+          );
+        else if (value.constructor === Object) {
+          const subRefs = this.#extractReferences(
+            value,
+            this.#appendPath(key, referencePath)
+          );
+
+          refs.columns.push(...subRefs.columns);
+          refs.tables.push(...subRefs.tables);
+          refs.wildcards.push(...subRefs.wildcards);
+        } else if (Object.prototype.toString.call(value) === '[object Array]') {
+          if (key === SQLElement.COLUMN_REFERENCE)
+            refs.columns.push(
+              this.#handleColumnIdentifierRef({
+                key,
+                value: value.join(''),
+                referencePath,
+              })
+            );
+          else
+            value.forEach((element: { [key: string]: any }) => {
+              const subRefs = this.#extractReferences(
+                element,
+                this.#appendPath(key, referencePath)
+              );
+
+              refs.columns.push(...subRefs.columns);
+              refs.tables.push(...subRefs.tables);
+              refs.wildcards.push(...subRefs.wildcards);
+            });
+        }
     });
-    return statementReferences;
+    return refs;
   };
 
-  static #getstatementReferences = (fileObj: any): StatementReference[][] => {
-    const statementReferences: StatementReference[][] = [];
+  // Runs through tree of parsed logic and extract all references of tables and columns (self and parent tables)
+  static #getStatementReferences = (fileObj: any): Refs[] => {
+    const statementReferences: Refs[] = [];
 
     if (
       fileObj.constructor === Object &&
       fileObj[SQLElement.STATEMENT] !== undefined
     ) {
-      const statementReferencesObj = this.#extractstatementReferences(
+      const statementReferencesObj = this.#extractReferences(
         fileObj[SQLElement.STATEMENT]
       );
       statementReferences.push(statementReferencesObj);
     } else if (Object.prototype.toString.call(fileObj) === '[object Array]') {
-      fileObj
-        .filter((statement: any) => SQLElement.STATEMENT in statement)
-        .forEach((statement: any) => {
-          const statementReferencesObj = this.#extractstatementReferences(
-            statement[SQLElement.STATEMENT]
-          );
-          statementReferences.push(statementReferencesObj);
-        });
+      const statementObjects = fileObj.filter(
+        (statement: any) => SQLElement.STATEMENT in statement
+      );
+
+      statementObjects.forEach((statement: any) => {
+        const statementReferencesObj = this.#extractReferences(
+          statement[SQLElement.STATEMENT]
+        );
+        statementReferences.push(statementReferencesObj);
+      });
     }
 
     return statementReferences;
@@ -239,7 +274,7 @@ export class Logic {
 
     const parsedLogicObj = JSON.parse(prototype.parsedLogic);
 
-    const statementReferences = this.#getstatementReferences(
+    const statementReferences = this.#getStatementReferences(
       parsedLogicObj.file
     );
 
