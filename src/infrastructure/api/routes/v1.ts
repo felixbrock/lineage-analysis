e57cd7ddf2ca6003d6e40964f558fd010a041272
Felix Brockmeier <felix.brockmeier@outlook.com>
2022-05-05 13:00:14 +0200

    added read logics endpoint

--- src/infrastructure/api/routes/v1.ts
+++ src/infrastructure/api/routes/v1.ts
@@ -14,6 +14,7 @@ v1Router.get('/', (req, res) => res.json({ message: "Yo! We're up!" }));
 
 v1Router.use(`/${apiRoot}/${version}/lineage`, lineageRoutes);
 v1Router.use(`/${apiRoot}/${version}/logic`, logicsRoutes);
+v1Router.use(`/${apiRoot}/${version}/logics`, logicsRoutes);
 v1Router.use(`/${apiRoot}/${version}/materializations`, materializationsRoutes);
 v1Router.use(`/${apiRoot}/${version}/columns`, columnsRoutes);
 v1Router.use(`/${apiRoot}/${version}/dependencies`, dependenciesRoutes);
