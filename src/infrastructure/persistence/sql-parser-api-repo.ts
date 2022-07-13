cd9ee5a45cc1a966875e3ee1149e675abc584469
nasir <nm219@ic.ac.uk>
2022-07-13 11:52:23 +0100

    added logic to create a placeholder whenever a parse error is encountered in an SQL file

--- src/infrastructure/persistence/sql-parser-api-repo.ts
+++ src/infrastructure/persistence/sql-parser-api-repo.ts
@@ -35,7 +35,10 @@ export default class SQLParserApiRepoImpl
       throw new Error(jsonResponse.message);
     } catch (error: unknown) {
       if (typeof error === 'string') return Promise.reject(error);
-      if (error instanceof Error) return Promise.reject(error.message);
+      if (error instanceof Error) {
+        if(error.message.includes('500')) return {file:[{}, {}]};
+        return Promise.reject(error.message);
+      }
       return Promise.reject(new Error('Unknown error occured'));
     }
   };
