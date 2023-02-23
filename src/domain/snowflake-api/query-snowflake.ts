cdd3a9246ccfc798f2f49e1167be983b30778510
feliciori <felix.brockmeier@outlook.com>
2023-02-23 19:02:00 +0100

    changes

--- src/domain/snowflake-api/query-snowflake.ts
+++ src/domain/snowflake-api/query-snowflake.ts
@@ -1,3 +1,4 @@
+import { Blob } from 'node:buffer';
 import Result from '../value-types/transient-types/result';
 
 import {
@@ -33,19 +34,65 @@ export class QuerySnowflake
     this.#snowflakeApiRepo = snowflakeApiRepo;
   }
 
+  #splitBinds = (queryTextSize: number, binds: Binds): Binds[] => {
+    // todo - Upload as file and then copy into table
+    const byteToMBDivisor = 1000000;
+    const maxQueryMBSize = 1;
+    const querySizeOffset = 0.1;
+    const maxQueryTextMBSize = 0.2;
+
+    const queryTextMBSize = queryTextSize / byteToMBDivisor;
+    const bindsSize = new Blob([JSON.stringify(binds)]).size;
+    const bindsMBSize = bindsSize / byteToMBDivisor;
+
+    if (queryTextMBSize + bindsMBSize < maxQueryMBSize * (1 - querySizeOffset))
+      return [binds];
+    if (queryTextMBSize > maxQueryTextMBSize)
+      throw new Error('Query text size too large. Implement file upload');
+
+    // in MB (subtracting offset)
+    const maxSize = 1 * (1 - querySizeOffset);
+    const maxBindsSequenceMBSize = maxSize - queryTextMBSize;
+
+    const numSequences = Math.ceil(bindsMBSize / maxBindsSequenceMBSize);
+    const numElementsPerSequence = Math.ceil(binds.length / numSequences);
+
+    const res: Binds[] = [];
+    for (let i = 0; i < binds.length; i += numElementsPerSequence) {
+      const chunk = binds.slice(i, i + numElementsPerSequence);
+      res.push(chunk);
+    }
+
+    return res;
+  };
+
   async execute(
     request: QuerySnowflakeRequestDto,
     auth: QuerySnowflakeAuthDto,
     connPool: IConnectionPool
   ): Promise<QuerySnowflakeResponseDto> {
     try {
-      const queryResult = await this.#snowflakeApiRepo.runQuery(
-        request.queryText,
-        request.binds,
-        connPool
+      console.log(request.queryText);
+      console.log(request.binds.length);
+
+      const bindSequences = this.#splitBinds(
+        new Blob([request.queryText]).size,
+        request.binds
       );
 
-      return Result.ok(queryResult);
+      const results = await Promise.all(
+        bindSequences.map(async (el) => {
+          const res = await this.#snowflakeApiRepo.runQuery(
+            request.queryText,
+            el,
+            connPool
+          );
+
+          return res;
+        })
+      );
+
+      return Result.ok(results.flat());
     } catch (error: unknown) {
       if (error instanceof Error) console.error(error.stack);
       else if (error) console.trace(error);
